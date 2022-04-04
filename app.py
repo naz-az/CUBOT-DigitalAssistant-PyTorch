09c9e0b9c63ccf7079774e90b05ad9442d7136a9
naz-az <johndoe@email.com>
2022-04-04 15:22:10 +0200

    aa

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from email import message
 from urllib import response
 from flask import Flask, render_template, request, jsonify
-# from flask_cors import CORS
+from flask_cors import CORS
 
 from chat import get_response
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 @app.get("/")
 def index_get():
